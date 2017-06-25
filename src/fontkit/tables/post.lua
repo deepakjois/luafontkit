9501d8cdaf4bcc2e94c09d031b385631cecbf811
Deepak Jois <deepak.jois@gmail.com>
2017-06-25 11:29:11 +0530

    porting tables/post.js

---
+++ src/fontkit/tables/post.lua
@@ -0,0 +1,34 @@
+local r = require('restructure')
+
+-- PostScript information
+return r.VersionedStruct.new(r.fixed32, {
+  header = { -- these fields exist at the top of all versions
+    { italicAngle =        r.fixed32 }, -- Italic angle in counter-clockwise degrees from the vertical.
+    { underlinePosition =  r.int16 },   -- Suggested distance of the top of the underline from the baseline
+    { underlineThickness = r.int16 },   -- Suggested values for the underline thickness
+    { isFixedPitch =       r.uint32 },  -- Whether the font is monospaced
+    { minMemType42 =       r.uint32 },  -- Minimum memory usage when a TrueType font is downloaded as a Type 42 font
+    { maxMemType42 =       r.uint32 },  -- Maximum memory usage when a TrueType font is downloaded as a Type 42 font
+    { minMemType1 =        r.uint32 },  -- Minimum memory usage when a TrueType font is downloaded as a Type 1 font
+    { maxMemType1 =        r.uint32 }   -- Maximum memory usage when a TrueType font is downloaded as a Type 1 font
+  },
+
+  [1] = {}, -- version 1 has no additional fields
+
+  [2] = {
+    { numberOfGlyphs = r.uint16 },
+    { glyphNameIndex = r.Array.new(r.uint16, 'numberOfGlyphs') },
+    { names =          r.Array.new(r.String.new(r.uint8)) }
+  },
+
+  [2.5] = {
+    { numberOfGlyphs = r.uint16 },
+    { offsets =        r.Array.new(r.uint8, 'numberOfGlyphs') }
+  },
+
+  [3] = {}, -- version 3 has no additional fields
+
+  [4] = {
+    { map = r.Array.new(r.uint32, function(t) return  t.parent.maxp.numGlyphs end) }
+  }
+})
