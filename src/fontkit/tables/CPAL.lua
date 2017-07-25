f047425a55abbf50047da93262f74b7979c80858
Deepak Jois <deepak.jois@gmail.com>
2017-07-25 08:42:34 +0530

    Sync up changes
    * Update to 5d4d7d0fa3e2b7609bb526a164f7bb219635bc8d

--- src/fontkit/tables/CPAL.lua
+++ src/fontkit/tables/CPAL.lua
@@ -7,11 +7,18 @@ local ColorRecord = r.Struct.new({
   { alpha = r.uint8 }
 })
 
-return r.Struct.new({
-  { version = r.uint16 },
-  { numPaletteEntries = r.uint16 },
-  { numPalettes = r.uint16 },
-  { numColorRecords = r.uint16 },
-  { colorRecords = r.Pointer.new(r.uint32, r.Array.new(ColorRecord, 'numColorRecords')) },
-  { colorRecordIndices = r.Array.new(r.uint16, 'numPalettes') }
+return r.VersionedStruct.new(r.uint16, {
+  header = {
+    { numPaletteEntries = r.uint16 },
+    { numPalettes = r.uint16 },
+    { numColorRecords = r.uint16 },
+    { colorRecords = r.Pointer.new(r.uint32, r.Array.new(ColorRecord, 'numColorRecords')) },
+    { colorRecordIndices = r.Array.new(r.uint16, 'numPalettes') }
+  },
+  [0] = {},
+  [1] = {
+    { offsetPaletteTypeArray = r.Pointer.new(r.uint32, r.Array.new(r.uint32, 'numPalettes')) },
+    { offsetPaletteLabelArray = r.Pointer.new(r.uint32, r.Array.new(r.uint16, 'numPalettes')) },
+    { offsetPaletteEntryLabelArray = r.Pointer.new(r.uint32, r.Array.new(r.uint16, 'numPaletteEntries')) }
+  }
 })
